653c67f62dd35e5e030df8708116f85ff398e6e7
James Thorne <james@jthorne.co.uk>
2019-01-28 23:20:54 +0000

    add prediction script changes

--- scripts/predict.sh
+++ scripts/predict.sh
@@ -4,14 +4,14 @@ export PYTHONPATH=src
 export MODEL=unc
 
 mkdir -pv fever2/predictions/$MODEL/full/$1
-mkdir -pv fever2/predictions/$MODEL/full/$1/$2-working
+mkdir -pv fever2/working/$MODEL/full/$1/$2-working
 
 
-if [ "$(wc -l < fever2/$1/$2)" -eq "$(wc -l < fever2/predictions/$MODEL/oracle/$1/$2)" ];
+if [ "$(wc -l < fever2/$1/$2)" -eq "$(wc -l < fever2/predictions/$MODEL/full/$1/$2)" ];
 then echo 'Skipping making predictions as this exists';
 else
     python src/pipeline/fast_pipeline.py \
         --in-file fever2/$1/$2 \
         --out-file fever2/predictions/$MODEL/full/$1/$2 \
-        --working-dir fever2/working_dir/$MODEL/full/$1/$2
+        --working-dir fever2/working/$MODEL/full/$1/$2-working
 fi;
