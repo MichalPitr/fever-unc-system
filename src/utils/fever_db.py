dfbb81c323078a716b19339a4525fc85a1492521
James Thorne <james@jthorne.co.uk>
2019-03-28 13:29:38 +0000

    added wiki_term_builder

--- src/utils/fever_db.py
+++ src/utils/fever_db.py
@@ -1,4 +1,5 @@
 import sqlite3
+from utils.wiki_term_builder import parse_pages_checks, read_data
 from utils import text_clean
 from tqdm import tqdm
 import config
@@ -125,3 +126,78 @@ def check_document_id(save_path):
             count += 1
 
     print(count)
+
+def save_file_to_db(file, cursor):
+    d_list = read_data(file)
+    batch_size = 20
+    for start in range(0, len(d_list), batch_size):
+        end = start + batch_size
+        buffer_list = []
+        for one_entity in d_list[start:end]:
+            this_item = parse_pages_checks(0, one_entity)
+            buffer_list.append((this_item['id'], this_item['text'], this_item['lines']))
+
+        insert_many(cursor, buffer_list)
+        del buffer_list
+        # print(buffer_list)
+
+
+def save_wiki_pages(save_path):
+    conn = sqlite3.connect(save_path)
+    c = conn.cursor()
+    for file in tqdm(config.WIKI_PAGE_PATH.iterdir()):
+        print(file)
+        save_file_to_db(file, cursor=c)
+        conn.commit()
+    conn.close()
+
+
+
+
+def get_all_doc_ids(save_path, max_ind=None):
+    conn = sqlite3.connect(save_path)
+    c = conn.cursor()
+    c.execute("SELECT * from documents")
+    id_list = []
+    count = 0
+    for pid, text, lines in tqdm(c, total=5416537):
+        text = text.strip()
+        if len(text) > 1 and len(lines) > 0:    # len(text) > 1 will leave out some error documents
+            id_list.append(pid)
+            count += 1
+
+        if max_ind is not None and count > max_ind:
+            break
+        # if len(text) == 1:
+        #     print(pid, text)
+
+        # print(convert_brc(pid))
+
+    # print(len(id_list))
+    # print(len(set(id_list)))
+    return id_list
+
+
+def build_sentences_table(save_path):
+    conn = sqlite3.connect(save_path)
+    c_doc = conn.cursor()
+    c_doc.execute("SELECT * from documents")
+
+    c_sent = conn.cursor()
+    count = 0
+
+    for pid, text, lines in tqdm(c_doc, total=5416537):
+        if len(text) > 1:
+            lines_items = json.loads(lines)
+            for line in lines_items:
+                # print(line['sentences'])
+                if line['sentences']:
+                    count += 1
+                    sent_pid = pid + '(-.-)' + str(line['line_num'])
+                    sent = line['sentences']
+                    h_links = json.dumps(line['h_links'])
+                    insert_one_sent(c_sent, (sent_pid, sent, h_links, pid))
+
+    print(count)
+    conn.commit()
+    conn.close()
