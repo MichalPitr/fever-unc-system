9760c431763d7f87c15d0854f39a590f4d97b561
James Thorne <james@jthorne.co.uk>
2019-03-28 15:25:08 +0000

    update config path for data root

--- src/config.py
+++ src/config.py
@@ -8,8 +8,10 @@ WIKI_PAGE_PATH= SHARED_ROOT / "wiki-pages"
 
 PRO_ROOT = SRC_ROOT.parent
 
-DATA_ROOT = PRO_ROOT / "nsmn-data"
+DATA_ROOT = PRO_ROOT / "data"
 FEVER_DB = DATA_ROOT / "fever.db"
+
+print(FEVER_DB)
 TOKENIZED_DOC_ID = DATA_ROOT / "tokenized_doc_id.json"
 
 WN_FEATURE_CACHE_PATH = DATA_ROOT / "wn_feature_p"
